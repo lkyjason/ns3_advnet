40168131712a00c4d3e081e17a09a03da323a0e0
Jason Lee <meiyingl@andrew.cmu.edu>
2020-03-08 18:31:53 +0000

    client should run on UE

--- mm_echo.cc
+++ mm_echo.cc
@@ -20,40 +20,26 @@ using namespace mmwave;
  NS_LOG_COMPONENT_DEFINE ("ECHOEXAMPLE");
 
  int main (int argc, char *argv[]) {
-	//LogComponentEnable ("TcpSocketBase", LOG_LEVEL_INFO);
      LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
      LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
     
     Config::SetDefault ("ns3::LteRlcUm::MaxTxBufferSize", UintegerValue (1024 * 1024));
 	Config::SetDefault ("ns3::MmWavePhyMacCommon::ResourceBlockNum", UintegerValue(1));
 	Config::SetDefault ("ns3::MmWavePhyMacCommon::ChunkPerRB", UintegerValue(72));
-	//double stopTime = 5.9;
-	//double simStopTime = 7.00;
-	Ipv4Address remoteHostAddr;
-
-	// Command line arguments
-	CommandLine cmd;
-	cmd.Parse(argc, argv);
 
 	Ptr<MmWaveHelper> mmwaveHelper = CreateObject<MmWaveHelper> ();
-	//mmwaveHelper->SetSchedulerType ("ns3::MmWaveFlexTtiMaxWeightMacScheduler");
 	mmwaveHelper->SetSchedulerType ("ns3::MmWaveFlexTtiMacScheduler");
 	Ptr<MmWavePointToPointEpcHelper>  epcHelper = CreateObject<MmWavePointToPointEpcHelper> ();
 	mmwaveHelper->SetEpcHelper (epcHelper);
 
-	ConfigStore inputConfig;
-	inputConfig.ConfigureDefaults();
-
-	// parse again so you can override default values from the command line
-	cmd.Parse(argc, argv);
-
 	Ptr<Node> pgw = epcHelper->GetPgwNode ();
 
-	// Create a single RemoteHost
+	// Create a remote server 
 	NodeContainer nodes;
-	nodes.Create (2);
+	nodes.Create (1);
 	InternetStackHelper internet;
 	internet.Install (nodes);
+    Ptr<Node> server = nodes.Get(0);
 
 	// Create the Internet
 	PointToPointHelper p2ph;
@@ -62,14 +48,13 @@ using namespace mmwave;
 	p2ph.SetChannelAttribute ("Delay", TimeValue (MicroSeconds (1)));
 
     // setup links
-	NetDeviceContainer clientDevices = p2ph.Install (pgw, nodes.Get(0));
-	NetDeviceContainer serverDevices = p2ph.Install (pgw, nodes.Get(1));
+	NetDeviceContainer serverDevices = p2ph.Install (pgw, server);
 
     // assign IPs
 	Ipv4AddressHelper ipv4h;
 	ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
-	Ipv4InterfaceContainer clientIF = ipv4h.Assign (clientDevices);
 	Ipv4InterfaceContainer serverIF = ipv4h.Assign (serverDevices);
+	Ipv4Address server_addr = serverIF.GetAddress (1);
 
     // setup routing
 	Ipv4StaticRoutingHelper ipv4RoutingHelper;
@@ -118,17 +103,17 @@ using namespace mmwave;
 
     // setup echo server
     UdpEchoServerHelper echoServer (9);
-    ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+    ApplicationContainer serverApps = echoServer.Install (server);
     serverApps.Start (Seconds (1.0));
     serverApps.Stop (Seconds (10.0));
 
     // setup echo client
-    UdpEchoClientHelper echoClient (serverIF.GetAddress (1), 9);
+    UdpEchoClientHelper echoClient (server_addr, 9);
     echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
     echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.)));
     echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-    ApplicationContainer clientApps = echoClient.Install (nodes.Get(0));
+    ApplicationContainer clientApps = echoClient.Install (ueNode);
     clientApps.Start (Seconds (2.0));
     clientApps.Stop (Seconds (10.0));
 
