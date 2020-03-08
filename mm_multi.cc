2d35859d5cde0a1989daa83051389e25d6aad8c9
Jason Lee <meiyingl@andrew.cmu.edu>
2020-03-08 19:20:54 +0000

    multiple echo clients

---
+++ mm_multi.cc
@@ -0,0 +1,137 @@
+#include "ns3/core-module.h"
+//#include "ns3/simulator-module.h"
+//#include "ns3/node-module.h"
+//#include "ns3/helper-module.h"
+#include "ns3/node-container.h"
+#include "ns3/net-device-container.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+
+#include "ns3/mmwave-helper.h"
+#include "ns3/epc-helper.h"
+#include "ns3/mmwave-point-to-point-epc-helper.h"
+#include "ns3/mobility-module.h"
+#include "ns3/config-store.h"
+
+using namespace ns3;
+using namespace mmwave;
+
+ NS_LOG_COMPONENT_DEFINE ("ECHOEXAMPLE");
+
+ int main (int argc, char *argv[]) {
+     LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+     LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    
+    Config::SetDefault ("ns3::LteRlcUm::MaxTxBufferSize", UintegerValue (1024 * 1024));
+	Config::SetDefault ("ns3::MmWavePhyMacCommon::ResourceBlockNum", UintegerValue(1));
+	Config::SetDefault ("ns3::MmWavePhyMacCommon::ChunkPerRB", UintegerValue(72));
+
+	Ptr<MmWaveHelper> mmwaveHelper = CreateObject<MmWaveHelper> ();
+	mmwaveHelper->SetSchedulerType ("ns3::MmWaveFlexTtiMacScheduler");
+	Ptr<MmWavePointToPointEpcHelper>  epcHelper = CreateObject<MmWavePointToPointEpcHelper> ();
+	mmwaveHelper->SetEpcHelper (epcHelper);
+
+	Ptr<Node> pgw = epcHelper->GetPgwNode ();
+
+	// Create a remote server 
+	NodeContainer nodes;
+	nodes.Create (1);
+	InternetStackHelper internet;
+	internet.Install (nodes);
+    Ptr<Node> server = nodes.Get(0);
+
+	// Create the Internet
+	PointToPointHelper p2ph;
+	p2ph.SetDeviceAttribute ("DataRate", DataRateValue (DataRate ("100Gb/s")));
+	p2ph.SetDeviceAttribute ("Mtu", UintegerValue (1500));
+	p2ph.SetChannelAttribute ("Delay", TimeValue (MicroSeconds (1)));
+
+    // setup links
+	NetDeviceContainer serverDevices = p2ph.Install (pgw, server);
+
+    // assign IPs
+	Ipv4AddressHelper ipv4h;
+	ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
+	Ipv4InterfaceContainer serverIF = ipv4h.Assign (serverDevices);
+	Ipv4Address server_addr = serverIF.GetAddress (1);
+
+    // setup routing
+	Ipv4StaticRoutingHelper ipv4RoutingHelper;
+	Ptr<Ipv4StaticRouting> clientStaticRouting = ipv4RoutingHelper.GetStaticRouting (nodes.Get(0)->GetObject<Ipv4> ());
+	clientStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
+	Ptr<Ipv4StaticRouting> serverStaticRouting = ipv4RoutingHelper.GetStaticRouting (nodes.Get(1)->GetObject<Ipv4> ());
+	serverStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
+
+	NodeContainer ueNodes;
+	NodeContainer enbNodes;
+	enbNodes.Create(1);
+	ueNodes.Create(2);
+
+	// Install Mobility Model
+	MobilityHelper enbmobility;
+	Ptr<ListPositionAllocator> enbPositionAlloc = CreateObject<ListPositionAllocator> ();
+	enbPositionAlloc->Add (Vector (0.0, 0.0, 0.0));
+	enbmobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+	enbmobility.SetPositionAllocator(enbPositionAlloc);
+	enbmobility.Install (enbNodes);
+
+	MobilityHelper uemobility;
+	Ptr<ListPositionAllocator> uePositionAlloc = CreateObject<ListPositionAllocator> ();
+	uePositionAlloc->Add (Vector (30.0, 0.0, 0.0));
+	uemobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+	uemobility.SetPositionAllocator(uePositionAlloc);
+	uemobility.Install (ueNodes);
+
+	// Install LTE Devices to the nodes
+	NetDeviceContainer enbDevs = mmwaveHelper->InstallEnbDevice (enbNodes);
+	NetDeviceContainer ueDevs = mmwaveHelper->InstallUeDevice (ueNodes);
+
+	// Install the IP stack on the UEs
+	// Assign IP address to UEs, and install applications
+	internet.Install (ueNodes);
+	Ipv4InterfaceContainer ueIpIface;
+	ueIpIface = epcHelper->AssignUeIpv4Address (NetDeviceContainer (ueDevs));
+
+	mmwaveHelper->AttachToClosestEnb (ueDevs, enbDevs);
+	mmwaveHelper->EnableTraces ();
+
+    // setup echo server
+    UdpEchoServerHelper echoServer (9);
+    ApplicationContainer serverApps = echoServer.Install (server);
+    serverApps.Start (Seconds (1.0));
+    serverApps.Stop (Seconds (10.0));
+
+    // setup echo client
+    UdpEchoClientHelper echoClient (server_addr, 9);
+    echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+    echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.)));
+    echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+	// Set the default gateway for the UE
+	Ptr<Node> ueNode1 = ueNodes.Get (0);
+	Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting (ueNode1->GetObject<Ipv4> ());
+	ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress (), 1);
+
+    ApplicationContainer clientApp1 = echoClient.Install (ueNode1);
+    clientApp1.Start (Seconds (2.0));
+    clientApp1.Stop (Seconds (10.0));
+
+    // Set the default gateway for the UE
+	Ptr<Node> ueNode2 = ueNodes.Get (1);
+	Ptr<Ipv4StaticRouting> ueStaticRouting2 = ipv4RoutingHelper.GetStaticRouting (ueNode2->GetObject<Ipv4> ());
+	ueStaticRouting2->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress (), 1);
+
+    ApplicationContainer clientApp2 = echoClient.Install (ueNode2);
+    clientApp2.Start (Seconds (2.0));
+    clientApp2.Stop (Seconds (10.0));
+
+
+
+    // start simulation
+    Simulator::Run ();
+
+    // cleanup
+    Simulator::Destroy ();
+    return 0;
+ }
